c56438a5ecffe3ab91f4cc2588269be49a6be204
mohdaaronlim <mohdaaron.22@ichat.sp.edu.sg>
2023-10-30 10:25:45 +0800

    lab3

--- Ex1/bmi.py
+++ Ex1/bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
     if bmi < 18.5:
         print("Underweight")
+        print("-1")
     elif bmi >= 18.5 and bmi <= 25.0:
         print("Normal")
+        print("0")
     elif bmi > 25.0:
         print("Overweight")
+        print("1")
 
 calculate_bmi(weight=57, height=1.73)
